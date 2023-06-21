c7a9b6723a900a69cac17baec8a3558e3b20660e
Jozsef Vincze <vincze.jozsef.develop@gmail.com>
2023-06-21 11:42:43 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
-html_style = 'css/style.css'
+## conf.py
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/style.css',
+]
